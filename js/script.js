8ada17a8f376a16b9f50a7318a8afa0ba0642edf
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-08-23 11:11:51 -0300

    lógica base das funções adicionadas
    funções e calculo basico

--- js/script.js
+++ js/script.js
@@ -1,44 +1,31 @@
-function add(num1,num2){
-    num1=Number(num1);
-    num2=Number(num2);
-    return num1+num2;
+
+function add(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return num1 + num2;
 }
 
-function subtract(num1,num2){
-    num1=Number(num1);
-    num2=Number(num2);
-    return 10-10;
+function subtract(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return num1 - num2;
 }
 
-function multiply(num1,num2){
-    num1=Number(num1);
-    num2=Number(num2);
-    return 10*10;
-}
-function divide(num1,num2){
-    num1=Number(num1);
-    num2=Number(num2);
-    if(num2===0){
-        return "infinito";
-    }
-    return(num1/num2);
+function multiply(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return num1 * num2;
 }
 
-let result = add(5, 10);
-console.log("Resultado da soma: " + result);
+function divide(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    return num2 === 0 ? "Erro: divisão por zero" : num1 / num2;
+}
 
-let num1 = 0;
-let num2 = 0;
-
-result = subtract(10, 5);
-console.log("Resultado da subtração: " + result);
-
-result = multiply(5, 10);
-console.log("Resultado da multiplicação: " + result);
-
-result = divide(10, 2);
-console.log("Resultado da divisão: " + result);
-
-result = divide(10,0);
-console.log("Resultado da divisão: " + result);
-
+// Variáveis globais
+let num1 = null;
+let num2 = null;
+let oper = null;
+let result = null;
+let overwrite = true;
